783f400e247803976c47d04ee7ee70d9e472a2fb
deepika2000 <48157532+deepika2000@users.noreply.github.com>
2020-02-10 22:34:02 +0530

    Add files via upload

---
+++ Calculator/src/main/java/Calci/Calculator/Calculator.java
@@ -0,0 +1,17 @@
+package Calci.Calculator;
+
+
+public class Calculator {
+	double add(double x, double y) {
+		return x + y;
+	}
+	double subtract(double x, double y) {
+		return x - y;
+	}
+	double multiply(double x, double y) {
+		return x * y;
+	}
+	double divide(double x, double y) {
+		return x / y;
+	}
+}
